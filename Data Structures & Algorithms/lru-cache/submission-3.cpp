63fb409096e57c1a4b9e2c963906ad3f34bc5e1f
akasho805 <96943885+akasho805@users.noreply.github.com>
2026-04-30 23:02:29 +0530

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.cpp
@@ -0,0 +1,80 @@
+class LRUCache {
+public:
+
+    struct Node {
+    int key, val;
+    Node* next;
+    Node* prev;
+    };
+
+    Node* head;
+    Node* tail;
+    int capacity;
+
+    unordered_map<int,Node*> mp;
+
+    LRUCache(int k) 
+    {
+        capacity = k;
+        mp.reserve(capacity);
+        head = new Node{0, 0, nullptr, nullptr};
+        tail = new Node{0, 0, nullptr, nullptr};
+
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    void insertAfterHead(Node *node)
+    {
+        node->next = head->next;
+        node->prev = head;
+
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    void remove(Node *node)
+    {
+        node->next->prev=node->prev;
+        node->prev->next=node->next;
+    }
+    int get(int key) {
+        if(mp.contains(key))
+        {
+            Node *node = mp[key];
+            remove(node);
+            insertAfterHead(node);
+            return node->val;
+        }
+        
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        // if(mp.contains(key))
+        //above works on latest cpp only
+        if(mp.find(key) != mp.end())
+        {
+            Node *node = mp[key];
+            node->val=value;
+            remove(node);
+            insertAfterHead(node);
+        }
+        else
+        {
+            // Node *node = new Node{key,value,nullptr,nullptr}; 
+            // dont use () as cpp will not find matching constsructor and you will have to define one
+            Node *node = new Node{key,value,nullptr,nullptr};
+            insertAfterHead(node);
+            mp[key] = node;
+
+            if (mp.size() > capacity) 
+            {
+                Node *lru = tail->prev;
+                remove(lru);
+                mp.erase(lru->key);
+                delete lru;
+            }
+        }
+    }
+};
