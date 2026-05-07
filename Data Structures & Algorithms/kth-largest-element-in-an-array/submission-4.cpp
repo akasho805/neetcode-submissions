d194a8af0f17930f3f5af95e69020c1a8ea938ed
akasho805 <96943885+akasho805@users.noreply.github.com>
2026-05-07 21:58:36 +0530

    Add: kth-largest-element-in-an-array - submission-4

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-4.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+    int partition(int left,int right, vector<int> &nums)
+    {
+        int pivot = nums[right];
+        int pivotIndex = left;
+
+        for(int i = left; i < right; i++) 
+        {
+            if(nums[i] <= pivot) 
+            {
+                swap(nums[i], nums[pivotIndex]);
+                pivotIndex++;
+            }
+        }
+
+        swap(nums[pivotIndex], nums[right]);
+
+        return pivotIndex;
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+
+        int left = 0,right = nums.size()-1;
+
+        while(true)
+        {
+            int pivot = partition(left,right,nums);
+
+            if(pivot == nums.size()-k)
+            {
+                return nums[pivot];
+            }
+            else if(pivot < nums.size()-k)
+            {
+                left = pivot+1;
+            }
+            else
+            {
+                right = pivot-1;
+            }
+        }
+
+
+        // heap soln
+        // priority_queue<int,vector<int>,greater<int>> minH;
+
+        // for(int x: nums)
+        // {
+        //     minH.push(x);
+        //     if(minH.size()>k)
+        //     {
+        //         minH.pop();
+        //     }
+        // }
+
+        // return minH.top();
+    }
+};
