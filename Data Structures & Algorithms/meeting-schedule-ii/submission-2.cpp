d07fba36a742a21f59d9115321df8619bef864b3
akasho805 <96943885+akasho805@users.noreply.github.com>
2026-05-28 22:03:23 +0530

    Add: meeting-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-2.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+
+        vector<int> st,en;
+
+        int n =intervals.size();
+
+        for(int i=0;i<n;i++)
+        {
+            st.push_back(intervals[i].start);
+            en.push_back(intervals[i].end);
+        }
+
+        sort(st.begin(),st.end());
+        sort(en.begin(),en.end());
+
+        int i =0,j=0;
+
+        int count = 0;
+
+        int maxCount = 0;
+
+        while(i<n && j<n)
+        {
+            if(st[i]<en[j])
+            {
+                i++;count++;
+            }
+            else if(st[i]>=en[j])
+            {
+                j++;count--;
+            }
+            maxCount= max(count,maxCount);
+        }
+
+        return maxCount;
+    }
+};
