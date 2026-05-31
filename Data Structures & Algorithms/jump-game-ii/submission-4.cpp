aefff56f8392ec7ce0d9ae04a4d08a2a6ea7bddb
akasho805 <96943885+akasho805@users.noreply.github.com>
2026-06-01 00:37:44 +0530

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        int n =nums.size();
+        int jumps = 0;
+        int left = 0,right = 0,maxReach = 0;
+
+        while(right<n-1)
+        {
+            while(left<=right)
+            {
+                if(nums[left]+left > maxReach)
+                {
+                    maxReach = nums[left]+left;
+                }
+                left++;
+            }
+            right = maxReach;
+            jumps++; 
+        }
+
+        return jumps;
+    }
+};
