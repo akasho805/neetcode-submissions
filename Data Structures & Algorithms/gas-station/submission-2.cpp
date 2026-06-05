f8df2ac75fa0106308471fcc2ccc9cae65984747
akasho805 <96943885+akasho805@users.noreply.github.com>
2026-06-05 16:38:58 +0530

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        
+        
+        int n =cost.size();
+        vector<int> sum(n);
+
+        int totalSum = 0;
+        int currSum =0;
+        int maxI =0;
+
+        for(int i=0;i<n;i++)
+        {
+            int diff = gas[i]-cost[i];
+            currSum += diff;
+            totalSum += diff;
+
+            if(currSum < 0)
+            {
+                currSum = 0;
+                maxI=i+1; 
+            }
+        }
+
+        return (totalSum<0) ? -1 : maxI ;
+
+    }
+};
