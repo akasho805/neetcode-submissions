2f3a37e9d53ccf20da0aae9769440d4768d3c4d9
akasho805 <96943885+akasho805@users.noreply.github.com>
2026-06-07 15:26:21 +0530

    Add: dota2-senate - submission-1

---
+++ Data Structures & Algorithms/dota2-senate/submission-1.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n =senate.length();
+        queue<int> q1,q2;
+
+        for(int i=0;i<n;i++)
+        {
+            if(senate[i]=='R')
+            {
+                q1.push(i);
+            }
+            else
+            {
+                q2.push(i);
+            }
+        }
+
+        while(!q1.empty() and !q2.empty())
+        {
+            if(q1.front()<q2.front())
+            {
+                q2.pop();
+                int el= q1.front();
+                q1.pop();
+                q1.push(el+n);
+            }
+            else
+            {
+                q1.pop();
+                int el= q2.front();
+                q2.pop();
+                q2.push(el+n);
+            }
+        }
+
+
+        // traverses banned senators as well
+        // int i =0;
+        // while(!q1.empty() and !q2.empty())
+        // {
+        //     if(senate[i]=='R' and q1.front()==i)
+        //     {
+        //         q2.pop();
+        //         int el= q1.front();
+        //         q1.pop();
+        //         q1.push(el);
+        //     }
+        //     else if(senate[i]=='D' and q2.front()==i)
+        //     {
+        //         q1.pop();
+        //         int el= q2.front();
+        //         q2.pop();
+        //         q2.push(el);
+        //     }
+        //     i++;
+        //     i=i%n;
+        // }
+
+        return (!q1.empty()) ? "Radiant" : "Dire";
+    }
+};
