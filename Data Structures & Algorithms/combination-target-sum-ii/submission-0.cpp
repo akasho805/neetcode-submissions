33b613d1b68b0ea1f2bf38bd57b1d95d98280e03
akasho805 <96943885+akasho805@users.noreply.github.com>
2026-07-09 00:13:07 +0530

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    unordered_map<int,bool> seen;
+
+    void dfs(vector<int>& nums,int i,int target,vector<int>& subset, vector<vector<int>>& res,int sum)
+    {
+        if(sum == target)
+        {
+            res.push_back(subset);
+            return;
+        }
+
+        if(i>=nums.size() || sum + nums[i] > target)
+        {
+            return;
+        }
+
+        subset.push_back(nums[i]);
+        dfs(nums,i+1,target,subset,res,sum+nums[i]);
+        subset.pop_back();    
+        while(i+1<nums.size() and nums[i+1]==nums[i])
+        {
+            i++;
+        }
+        dfs(nums,i+1,target,subset,res,sum);
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        
+        sort(nums.begin(),nums.end());
+
+        vector<vector<int>> res;
+        vector<int> subset;
+        int sum=0;
+
+        dfs(nums,0,target,subset,res,sum);
+    
+        return res;
+
+    }
+};
