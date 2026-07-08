ecf1abf7887e1153cc51673a7117410e5d59b6f0
akasho805 <96943885+akasho805@users.noreply.github.com>
2026-07-09 00:35:44 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    void dfs(int i,int n,int k,vector<int>& combo,vector<vector<int>>& res)
+    {
+        //base case1
+        if(combo.size()==k)
+        {
+            res.push_back(combo);
+            return;
+        }
+
+        //base case2
+        if(i>n)
+        {
+            return;
+        }
+
+        //pruning
+        if(k-combo.size() > n-i+1 )
+        {
+            return;
+        }
+        
+        combo.push_back(i);
+        dfs(i+1,n,k,combo,res);
+        combo.pop_back();
+        dfs(i+1,n,k,combo,res);
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        
+        vector<int> combo;
+        vector<vector<int>> res;
+
+        dfs(1,n,k,combo,res);
+
+        return res;
+    }
+};
