3f8a135de2468853e5ef81b80def1891170817db
akasho805 <96943885+akasho805@users.noreply.github.com>
2026-07-20 23:54:47 +0530

    Add: permutations-ii - submission-3

---
+++ Data Structures & Algorithms/permutations-ii/submission-3.cpp
@@ -0,0 +1,77 @@
+//using hash set
+class Solution {
+public:
+    void dfs(int first, vector<int>& nums, vector<vector<int>>& res)
+    {
+        if (first == nums.size())
+        {
+            res.push_back(nums);
+            return;
+        }
+
+        unordered_set<int> seen;
+
+        for (int i = first; i < nums.size(); i++)
+        {
+            // Already placed this value at index 'first'
+            if (seen.count(nums[i]))
+                continue;
+
+            seen.insert(nums[i]);
+
+            swap(nums[first], nums[i]);      // Choose
+
+            dfs(first + 1, nums, res);       // Recurse
+
+            swap(nums[first], nums[i]);      // Undo
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums)
+    {
+        vector<vector<int>> res;
+        dfs(0, nums, res);
+        return res;
+    }
+};
+
+
+// using sort and modify array
+// class Solution {
+// public:
+
+//     void dfs(int first, vector<int>& nums, vector<vector<int>>& res)
+//     {
+//         if (first == nums.size())
+//         {
+//             res.push_back(nums);
+//             return;
+//         }
+
+//         for (int i = first; i < nums.size(); i++)
+//         {
+//             if(i > first && nums[i] == nums[first])
+//             {
+//                 continue;
+//             }
+
+//             swap(nums[first], nums[i]);      // Choose
+            
+//             dfs(first + 1, nums, res);       // Recurse
+
+//         }
+
+//         for (int i = nums.size() - 1; i > first; --i) 
+//         {
+//             swap(nums[first], nums[i]);      // Undo
+//         }
+//     }
+
+//     vector<vector<int>> permuteUnique(vector<int>& nums)
+//     {
+//         sort(nums.begin(), nums.end());
+//         vector<vector<int>> res;
+//         dfs(0, nums, res);
+//         return res;
+//     }
+// };
